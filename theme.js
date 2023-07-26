11ca21ac6bebe035a6dd8f59f2eef15478cd4bba
fdfiroz <firozahamed0@gmail.com>
2023-07-26 12:24:27 +0600

    Upto Banner

---
+++ theme.js
@@ -0,0 +1,7 @@
+const theme = { borderRadius: 
+   { none: '0',
+     xs: '0.25rem',
+     sm: '0.5rem',
+     default: '1.5rem',
+     lg: '3.125rem',
+     full: '9999px' } }; export default theme;
